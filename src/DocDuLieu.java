b64f16da1180401fc8b311620147e679effe0301
Anh <anh@fpt.edu.vn>
2019-03-21 06:01:05 +0700

    nhap_gia_tri_tu_ban_phim(Scaner..)

---
+++ src/DocDuLieu.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class DocDuLieu {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("vui long nhap du lieu vao ban phim:");
+        int a = scanner.nextInt();
+        System.out.println("vui long nhap so a vao ban phim:" + a);
+    }
+}
